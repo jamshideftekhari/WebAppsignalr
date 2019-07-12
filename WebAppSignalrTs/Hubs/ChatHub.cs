3c0dd4f3124a14b05222de259cc9051412534f43
Jamshid Eftekhari <jaef@easj.dk>
2019-07-12 11:09:37 +0200

    Add project files.

---
+++ WebAppSignalrTs/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAppSignalrTs.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
